86e0d8753d48ee72074d68d81fe20b0eb3e55447
Jlucas-4 <jlucaspemaciel@gmail.com>
2024-10-17 08:08:31 -0300

    save q era pa ter ido ontem

---
+++ AppSenhaJL&LH/Repository/ColaboradorRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace AppSenhaJL_LH.Repository
+{
+    public class ColaboradorRepository
+    {
+        private readonly string _conexaoMySQL;
+
+        public ColaboradorRepository(IConfiguration conf)
+        {
+            _conexaoMySQL = conf.GetConnectionString("ConexaoMySQL");
+        }
+    }
+}
---
+++ AppSenhaJL&LH/Repository/Contract/IColaboradorRepository.cs
@@ -0,0 +1,20 @@
+﻿using AppSenhaJL_LH.Models;
+
+namespace AppSenhaJL_LH.Repository.Contract
+{
+    public interface IColaboradorRepository
+    {
+   
+            // login Cliente 
+
+            Colaborador Login(string Email, string Senha);
+
+            // CRUD
+            void Cadastrar(Colaborador colaborador);
+            void Atualizar(Colaborador colaborador);
+            void Excluir(int Id);
+            Colaborador ObterCliente(int Id);
+            IEnumerable<Colaborador> ObterTodosClientes();
+            // IPagedList<cliente> ObterTodosClientes(int? pagina, string pesquisa);
+    }
+}
